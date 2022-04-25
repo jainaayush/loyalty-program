145b7bef99bcbdc67bd5e313c5aba88f6db5b4d5
jainaayush <aayushj.pis@gmail.com>
2022-04-25 16:24:05 +0530

    Code refactore

--- db/migrate/20220422063730_create_invoices.rb
+++ db/migrate/20220422063730_create_invoices.rb
@@ -1,12 +1,11 @@
 class CreateInvoices < ActiveRecord::Migration[7.0]
   def change
     create_table :invoices do |t|
-
-      t.integer  :amount
-      t.datetime :invoice_date
       t.references :user, index: true, foreign_key: true
-      t.boolean :foreign_country_investment
-      t.timestamps
+      t.boolean :foreign_country_investment, default: false
+      t.decimal :amount, null: false, precision: 10, scale: 2
+      t.datetime :invoice_date, null: false
+      t.timestamps null: false
     end
   end
 end
